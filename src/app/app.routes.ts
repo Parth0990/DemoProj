bb61f28464be9f70937a970514cbf256d50240e3
Parth0990 <92087989+Parth0990@users.noreply.github.com>
2024-04-18 09:04:05 +0530

    Fifth Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +17,3 @@ export const routes: Routes = [{
     component: HomeComponent,
     pathMatch: 'full'
 }];
-=======
-
-export const routes: Routes = [];
->>>>>>> 0b304bbfe91918fcae8c83be983a46eb7b47c03e
