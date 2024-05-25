fd2619270281b7edc3bc22650e2156545828af16
Parth0990 <92087989+Parth0990@users.noreply.github.com>
2024-05-25 19:37:48 +0530

    25th Commit

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -11,10 +11,10 @@ export class PurchaseModel {
   ItemName: string = '';
   ItemGroupId: number = 0;
   Batch: string = '';
-  EffectiveDate: Date = new Date();
+  PurchaseDate: Date = new Date();
   Terms: number = 0;
   DesignNo: string = '';
-  VenderId: number = 0;
+  VendorId: number = 0;
   Series: number = 0;
   Balance: string = '';
   BillNo: string = '';
@@ -52,6 +52,7 @@ export class PurchaseComponent implements OnInit {
   ItemDataSource: PurchaseModel[] = [];
   PurchaseDataSource: [] = [];
   IsAddSection: boolean = false;
+  OperationType: string = '';
   displayedColumns = [
     'ItemName',
     'DesignNo',
@@ -80,6 +81,7 @@ export class PurchaseComponent implements OnInit {
     'DiscountAmt',
     'TaxAmt',
     'NetValue',
+    'Action',
   ];
 
   constructor(private DBService: DBService) {}
@@ -98,10 +100,14 @@ export class PurchaseComponent implements OnInit {
   }
 
   GetPurchasedData() {
-    this.Qry = `Select * FROM purchaseSummary PS
+    this.Qry = `SELECT PS.PurchaseId,VM.VendorId,VM.VendorName,IM.ItemGroupId,IM.ItemName,Cast(PS.PurchaseDate as char(11)) As PurchaseDate
+                ,PS.Terms, PS.Series, IM.StockQty, IM.DesignNo, PS.Balance, PD.MainQty, PD.AltQty, PD.Free, PD.Per, PD.BasicQty
+                , PD.Discount, PD.DiscountAmt, PD.TaxAmt, PD.NetValue
+                FROM purchaseSummary PS
                 INNER JOIN VendorMaster VM ON VM.VendorId = PS.VendorId
                 INNER JOIN purchasedetail PD ON PD.purchaseId = PD.purchaseId
-                INNER JOIN ItemMaster IM ON IM.ItemId = PD.ItemId;`;
+                INNER JOIN ItemMaster IM ON IM.ItemId = PD.ItemId
+                INNER JOIN ItemGroupId IG ON IG.ItemGroupId = PS.ItemGroupId;`;
     let data = this.DBService.query(this.Qry);
     setTimeout(() => {
       if (data.IsErrorExists) {
@@ -156,7 +162,7 @@ export class PurchaseComponent implements OnInit {
   OnVendorChange() {
     this.Qry =
       'select * from VendorMaster WHERE VendorId = ' +
-      this.PurchaseModelData.VenderId;
+      this.PurchaseModelData.VendorId;
     let data = this.DBService.query(this.Qry);
     setTimeout(() => {
       if (data.IsErrorExists) {
@@ -225,88 +231,194 @@ export class PurchaseComponent implements OnInit {
     }, 500);
   }
 
-  AddNewRecord() {
-    let data: DBServiceModel = new DBServiceModel();
-
-    this.Qry =
-      `SELECT * FROM purchasesummary WHERE VendorId = ` +
-      this.PurchaseModelData.VenderId +
-      " AND purchasedate = '" +
-      this.PurchaseModelData.EffectiveDate +
-      "'";
-    let CheckRecord = this.DBService.query(this.Qry);
-    setTimeout(() => {
-      if (CheckRecord.QueryResultData && CheckRecord.QueryResultData.length > 0) {
-        let table = JSON.parse(JSON.stringify(CheckRecord.QueryResultData));
-        this.PurchaseModelData.PurchaseId = table[0].purchaseId;
-        data = this.InsertIntoDetail();
-        setTimeout(() => {
-          if(data.IsErrorExists){
-            Swal.fire(data.ErrorMessgae, "", "error");
-          }
-          else{
-            Swal.fire('Record Added!!', '', 'success');
-          }
-        }, 500);
-      } else {
-        this.Qry = `INSERT INTO purchasesummary(purchasedate, terms, vendorId, series, balance) VALUES(?, ?, ?, ?, ?);`;
-        this.parameters = [
-          this.PurchaseModelData.EffectiveDate,
-          this.PurchaseModelData.Terms,
-          this.PurchaseModelData.VenderId,
-          this.PurchaseModelData.Series,
-          this.PurchaseModelData.Balance,
-        ];
-  
-        data = this.DBService.InsertUpdateTables(this.Qry, this.parameters);
-  
-        setTimeout(() => {
-          if (data.IsErrorExists) {
-            Swal.fire(data.ErrorMessgae);
-          } else {
-            this.PurchaseModelData.PurchaseId = JSON.parse(
-              JSON.stringify(data.QueryResultData)
-            )['insertId'];
-            this.Qry = "";
-            data = this.InsertIntoDetail();
-            setTimeout(() => {
-              if (data.IsErrorExists) {
-                Swal.fire(data.ErrorMessgae);
-              } else {
-                Swal.fire('Record Added!!', '', 'success');
-                this.GetPurchasedData();
-                this.IsAddSection = !this.IsAddSection;
-              }
-            }, 500);
-          }
-        }, 500);
-      }
-    }, 500);
+  CrudPanel(element: any, Action: string = '') {
+    this.PurchaseModelData = Object.assign({}, element);
+    this.PurchaseArrModel = [];
+    this.PurchaseArrModel.push(
+      JSON.parse(JSON.stringify(this.PurchaseArrModel))
+    );
+    if (this.PurchaseArrModel && this.PurchaseArrModel.length > 0) {
+      this.ItemDataSource = JSON.parse(JSON.stringify(this.PurchaseArrModel));
+      //this.PurchaseModelData += this.PurchaseModelData.NetValue;
+    } else {
+      this.ItemDataSource = [];
+    }
+    if (Action.toLowerCase() == 'edit') {
+      this.IsAddSection = true;
+      this.OperationType = Action;
+    }
+    if (Action.toLowerCase() == 'delete') {
+      Swal.fire({
+        title: 'Are you sure?',
+        icon: 'info',
+        confirmButtonText: 'Yes',
+        showCloseButton: true,
+      }).then((result) => {
+        if (result['isConfirmed']) {
+          this.Qry =
+            'Delete From SalesDetail WHERE PurchaseId = ' +
+            this.PurchaseModelData.PurchaseId;
+          let data = this.DBService.query(this.Qry);
+          setTimeout(() => {
+            if (data.IsErrorExists) {
+              Swal.fire(data.ErrorMessgae, '', 'error');
+            } else {
+              this.Qry =
+                'DELETE FROM SalesSummary WHERE PurchaseId = ' +
+                this.PurchaseModelData.PurchaseId;
+              data = this.DBService.query(this.Qry);
+              setTimeout(() => {
+                if (data.IsErrorExists) {
+                  Swal.fire(data.ErrorMessgae, '', 'error');
+                } else {
+                  Swal.fire('Deleted!!', '', 'success');
+                  this.GetPurchasedData();
+                  this.IsAddSection = false;
+                }
+              }, 500);
+            }
+          }, 500);
+          console.log(data);
+        }
+      });
+      this.OperationType = Action;
+    }
   }
 
-  InsertIntoDetail(): DBServiceModel {
+  AddNewRecord() {
+    let data: DBServiceModel = new DBServiceModel();
+    if (this.OperationType.toLowerCase() == 'edit') {
+      this.Qry =
+        `UPDATE SalesSummary SET VendorId = ?, PurchaseDate = ?, Terms = ?, Balance = ? WHERE SalesId = ` +
+        this.PurchaseModelData.PurchaseId;
+      this.parameters = [
+        this.PurchaseModelData.VendorId,
+        this.PurchaseModelData.PurchaseDate,
+        this.PurchaseModelData.Terms,
+        this.PurchaseModelData.Balance,
+      ];
+
+      let data = this.DBService.InsertUpdateTables(this.Qry, this.parameters);
+      setTimeout(() => {
+        if (data.IsErrorExists) {
+          Swal.fire(data.ErrorMessgae, '', 'error');
+        } else {
+          data = this.InsertUpdateIntoDetail();
+          setTimeout(() => {
+            if (data.ErrorMessgae) {
+              Swal.fire(data.ErrorMessgae, '', 'error');
+            } else {
+              Swal.fire('Record Updated!!!', '', 'success');
+              this.IsAddSection = false;
+              this.GetPurchasedData();
+            }
+          }, 500);
+        }
+      }, 500);
+    } else {
+      this.Qry =
+        `SELECT * FROM purchasesummary WHERE VendorId = ` +
+        this.PurchaseModelData.VendorId +
+        " AND purchasedate = '" +
+        this.PurchaseModelData.PurchaseDate +
+        "'";
+      let CheckRecord = this.DBService.query(this.Qry);
+      setTimeout(() => {
+        if (
+          CheckRecord.QueryResultData &&
+          CheckRecord.QueryResultData.length > 0
+        ) {
+          let table = JSON.parse(JSON.stringify(CheckRecord.QueryResultData));
+          this.PurchaseModelData.PurchaseId = table[0].purchaseId;
+          data = this.InsertUpdateIntoDetail();
+          setTimeout(() => {
+            if (data.IsErrorExists) {
+              Swal.fire(data.ErrorMessgae, '', 'error');
+            } else {
+              Swal.fire('Record Added!!', '', 'success');
+            }
+          }, 500);
+        } else {
+          this.Qry = `INSERT INTO purchasesummary(purchasedate, terms, vendorId, series, balance) VALUES(?, ?, ?, ?, ?);`;
+          this.parameters = [
+            this.PurchaseModelData.PurchaseDate,
+            this.PurchaseModelData.Terms,
+            this.PurchaseModelData.VendorId,
+            this.PurchaseModelData.Series,
+            this.PurchaseModelData.Balance,
+          ];
+
+          data = this.DBService.InsertUpdateTables(this.Qry, this.parameters);
+
+          setTimeout(() => {
+            if (data.IsErrorExists) {
+              Swal.fire(data.ErrorMessgae);
+            } else {
+              this.PurchaseModelData.PurchaseId = JSON.parse(
+                JSON.stringify(data.QueryResultData)
+              )['insertId'];
+              this.Qry = '';
+              data = this.InsertUpdateIntoDetail();
+              setTimeout(() => {
+                if (data.IsErrorExists) {
+                  Swal.fire(data.ErrorMessgae);
+                } else {
+                  Swal.fire('Record Added!!', '', 'success');
+                  this.GetPurchasedData();
+                  this.IsAddSection = !this.IsAddSection;
+                }
+              }, 500);
+            }
+          }, 500);
+        }
+      }, 500);
+    }
+  }
+
+  InsertUpdateIntoDetail(): DBServiceModel {
     let data: DBServiceModel = new DBServiceModel();
     if (this.PurchaseArrModel && this.PurchaseArrModel.length > 0) {
       for (let i = 0; i < this.PurchaseArrModel.length; i++) {
-        this.Qry = `INSERT INTO PurchaseDetail(purchaseId, itemId, Batch, MainQty, altqty, free, per, basicqty, discount, discountamt, taxamt, netvalue) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-        this.parameters = [
-          this.PurchaseModelData.PurchaseId,
-          this.PurchaseArrModel[i].ItemGroupId,
-          this.PurchaseArrModel[i].Batch,
-          this.PurchaseArrModel[i].MainQty,
-          this.PurchaseArrModel[i].AltQty,
-          this.PurchaseArrModel[i].Price,
-          this.PurchaseArrModel[i].Per,
-          this.PurchaseArrModel[i].BasicAmt,
-          this.PurchaseArrModel[i].Discount,
-          this.PurchaseArrModel[i].DiscountAmt,
-          this.PurchaseArrModel[i].TaxAmt,
-          this.PurchaseArrModel[i].NetValue,
-        ];
-        data = this.DBService.InsertUpdateTables(this.Qry, this.parameters);
-        if (data.IsErrorExists) {
-          Swal.fire('Error: ' + data.ErrorMessgae, '', 'error');
-          break;
+        if (this.OperationType.toLowerCase() == 'edit') {
+          this.Qry =
+            `UPDATE SalesDetail SET ItemGroupId = ?, Batch = ?, MainQty = ?, AltQty = ?, Price = ?, Per = ?, 
+             BasicAmt = ?, Discount = ?, DiscountAmt = ?, TaxAmt = ?, NetValue = ?
+             WHERE SalesId = ` + this.PurchaseModelData.PurchaseId;
+
+          this.parameters = [
+            this.PurchaseArrModel[i].ItemGroupId,
+            this.PurchaseArrModel[i].Batch,
+            this.PurchaseArrModel[i].MainQty,
+            this.PurchaseArrModel[i].AltQty,
+            this.PurchaseArrModel[i].Price,
+            this.PurchaseArrModel[i].Per,
+            this.PurchaseArrModel[i].BasicAmt,
+            this.PurchaseArrModel[i].Discount,
+            this.PurchaseArrModel[i].DiscountAmt,
+            this.PurchaseArrModel[i].TaxAmt,
+            this.PurchaseArrModel[i].NetValue,
+          ];
+        } else {
+          this.Qry = `INSERT INTO PurchaseDetail(purchaseId, itemId, Batch, MainQty, altqty, free, per, basicqty, discount, discountamt, taxamt, netvalue) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+          this.parameters = [
+            this.PurchaseModelData.PurchaseId,
+            this.PurchaseArrModel[i].ItemGroupId,
+            this.PurchaseArrModel[i].Batch,
+            this.PurchaseArrModel[i].MainQty,
+            this.PurchaseArrModel[i].AltQty,
+            this.PurchaseArrModel[i].Price,
+            this.PurchaseArrModel[i].Per,
+            this.PurchaseArrModel[i].BasicAmt,
+            this.PurchaseArrModel[i].Discount,
+            this.PurchaseArrModel[i].DiscountAmt,
+            this.PurchaseArrModel[i].TaxAmt,
+            this.PurchaseArrModel[i].NetValue,
+          ];
+          data = this.DBService.InsertUpdateTables(this.Qry, this.parameters);
+          if (data.IsErrorExists) {
+            Swal.fire('Error: ' + data.ErrorMessgae, '', 'error');
+            break;
+          }
         }
       }
       return data;
